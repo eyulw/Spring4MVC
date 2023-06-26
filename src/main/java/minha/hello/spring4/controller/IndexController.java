7a39e7d3970973261d8f87564b9c42520649fb84
eyulw <alsgk6989@gmail.com>
2023-06-26 13:53:31 +0900

    IndexController.java에 Model 객체 생성, sayHello 변수 추가

--- src/main/java/minha/hello/spring4/controller/IndexController.java
+++ src/main/java/minha/hello/spring4/controller/IndexController.java
@@ -1,6 +1,7 @@
 package minha.hello.spring4.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -8,7 +9,13 @@ public class IndexController {
 
 //    http://localhost:8080/ 호출시 실행
     @RequestMapping("/")
-    public String index(){
+    public String index(Model m){
+        
+//        addAttribute(변수명, 대상객체)
+//        sayHello라는 변수명으로 대상객체 호출
+        m.addAttribute("sayHello","Hello World! from Controller");
+        
         return "index";
     }
+    
 }
