22c99af4ee572460f721886ba44b17ce3753d67f
eyulw <alsgk6989@gmail.com>
2023-06-26 14:40:40 +0900

    logging 사용하기

--- src/main/java/minha/hello/spring4/controller/IndexController.java
+++ src/main/java/minha/hello/spring4/controller/IndexController.java
@@ -1,12 +1,18 @@
 package minha.hello.spring4.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
+
 @Controller
 public class IndexController {
 
+//    로거 설정 : getLogger(로깅할클래스명)
+    private Logger logger = LogManager.getLogger(IndexController.class);
 //    http://localhost:8080/ 호출시 실행
     @RequestMapping("/")
     public String index(Model m){
@@ -14,7 +20,9 @@ public class IndexController {
 //        addAttribute(변수명, 대상객체)
 //        sayHello라는 변수명으로 대상객체 호출
         m.addAttribute("sayHello","Hello World! from Controller");
-        
+
+//        로거 출력
+        logger.info("Hello, World!! from Logger!");
         return "index";
     }
     
