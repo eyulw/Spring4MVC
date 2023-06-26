beecfeeea0c28c3e128ff033711a60ff54823fbe
eyulw <alsgk6989@gmail.com>
2023-06-26 17:40:10 +0900

    DTO/VO 클래스에 lombok 적용

--- src/main/java/minha/hello/spring4/model/Board.java
+++ src/main/java/minha/hello/spring4/model/Board.java
@@ -1,5 +1,11 @@
 package minha.hello.spring4.model;
 
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
 public class Board {
     private String bno;
     private String title;
@@ -7,73 +13,4 @@ public class Board {
     private String contents;
     private String views;
     private String regdate;
-
-    public Board(String bno, String title, String userid, String contents, String views, String regdate) {
-        this.bno = bno;
-        this.title = title;
-        this.userid = userid;
-        this.contents = contents;
-        this.views = views;
-        this.regdate = regdate;
-    }
-
-    public String getBno() {
-        return bno;
-    }
-
-    public void setBno(String bno) {
-        this.bno = bno;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getUserid() {
-        return userid;
-    }
-
-    public void setUserid(String userid) {
-        this.userid = userid;
-    }
-
-    public String getContents() {
-        return contents;
-    }
-
-    public void setContents(String contents) {
-        this.contents = contents;
-    }
-
-    public String getViews() {
-        return views;
-    }
-
-    public void setViews(String views) {
-        this.views = views;
-    }
-
-    public String getRegdate() {
-        return regdate;
-    }
-
-    public void setRegdate(String regdate) {
-        this.regdate = regdate;
-    }
-
-    @Override
-    public String toString() {
-        return "Board{" +
-                "bno='" + bno + '\'' +
-                ", title='" + title + '\'' +
-                ", userid='" + userid + '\'' +
-                ", contents='" + contents + '\'' +
-                ", views='" + views + '\'' +
-                ", regdate='" + regdate + '\'' +
-                '}';
-    }
 }
