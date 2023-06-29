d75f197760908647846c770f571e44fa7ab8ddce
eyulw <alsgk6989@gmail.com>
2023-06-29 16:50:32 +0900

    MemberService readOneMember 구현

--- src/main/java/minha/hello/spring4/service/MemberService.java
+++ src/main/java/minha/hello/spring4/service/MemberService.java
@@ -5,4 +5,6 @@ import minha.hello.spring4.model.Member;
 public interface MemberService {
     boolean saveMember(Member m);
     boolean loginMember(Member m);
+
+    Member readOneMember(String userid);
 }
--- src/main/java/minha/hello/spring4/service/MemberServiceImpl.java
+++ src/main/java/minha/hello/spring4/service/MemberServiceImpl.java
@@ -26,4 +26,9 @@ public class MemberServiceImpl implements MemberService{
 
         return isLogin;
     }
+
+    @Override
+    public Member readOneMember(String userid) {
+        return mdao.selectOneMember(userid);
+    }
 }
