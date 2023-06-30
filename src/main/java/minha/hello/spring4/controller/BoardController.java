08f134b666675b0c61569749a3c7a0d30d1285e0
eyulw <alsgk6989@gmail.com>
2023-06-30 16:54:49 +0900

    BoardController : list 구현

--- src/main/java/minha/hello/spring4/controller/BoardController.java
+++ src/main/java/minha/hello/spring4/controller/BoardController.java
@@ -1,7 +1,9 @@
 package minha.hello.spring4.controller;
 
+import minha.hello.spring4.service.BoardService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,17 +14,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/board")
 public class BoardController {
 
+    @Autowired BoardService bsrv;
     private Logger logger = LogManager.getLogger(BoardController.class);
     @GetMapping("/list")
-    public String list(){
-        
+    public String list(Model m, int cpg){
+        m.addAttribute("boards",bsrv.readBoard(cpg));
+
         logger.info("board/list 호출!");
         return "board/list.tiles";
     }
 
     @GetMapping("/write")
     public String write(){
-
         logger.info("board/write 호출!");
         return "board/write.tiles";
     }
