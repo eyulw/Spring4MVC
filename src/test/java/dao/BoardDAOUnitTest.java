f71cf1a5b9eb4e2000b939168de3cf47210c8f40
eyulw <alsgk6989@gmail.com>
2023-07-03 14:52:17 +0900

    BoardDAO : insertBoard 단위테스트

--- src/test/java/dao/BoardDAOUnitTest.java
+++ src/test/java/dao/BoardDAOUnitTest.java
@@ -2,12 +2,14 @@ package dao;
 
 import minha.hello.spring4.dao.BoardDAO;
 import minha.hello.spring4.model.Board;
+import minha.hello.spring4.model.Member;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -44,5 +46,13 @@ public class BoardDAOUnitTest {
         Board result=bdao.selectOneBoard(bno);
         assertNotNull(result);
         System.out.println(result);
+    }
+
+    @Test
+    @Transactional  // 데이터 입력후 자동삭제
+    public void insertBoard() throws Exception{
+        Board bd = new Board(null,"새글","abc123","테스트",null,null);
+//        assertEquals(테스트메서드, 검증값)
+        assertEquals(bdao.insertBoard(bd),1);
     }
 }
