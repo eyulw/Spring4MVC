fa765661c51c94fb1af7de09278bdc0d14848da4
eyulw <alsgk6989@gmail.com>
2023-07-03 14:53:57 +0900

    BoardController : writeok 구현

--- src/main/java/minha/hello/spring4/controller/BoardController.java
+++ src/main/java/minha/hello/spring4/controller/BoardController.java
@@ -1,5 +1,6 @@
 package minha.hello.spring4.controller;
 
+import minha.hello.spring4.model.Board;
 import minha.hello.spring4.service.BoardService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -31,6 +33,16 @@ public class BoardController {
     public String write(){
         logger.info("board/write 호출!");
         return "board/write.tiles";
+    }
+
+    @PostMapping("/write")
+    public String writeok(Board bd){
+        logger.info("board/writeok 호출!");
+        String returnPage="redirect:/board/fail";
+        if(bsrv.saveBoard(bd)){
+            returnPage = "redirect:/board/list?cpg=1";
+        }
+        return returnPage;
     }
     @GetMapping("/view")
     public String view(Model m,String bno){
