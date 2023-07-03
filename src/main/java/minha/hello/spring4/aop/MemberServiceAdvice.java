24849ac08f9bd8b5e95c28336d4f4f72d348b3a1
eyulw <alsgk6989@gmail.com>
2023-07-03 16:59:35 +0900

    join에 대해 포인트컷, 어드바이스 추가

--- src/main/java/minha/hello/spring4/aop/MemberServiceAdvice.java
+++ src/main/java/minha/hello/spring4/aop/MemberServiceAdvice.java
@@ -6,6 +6,7 @@ import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpSession;
@@ -15,8 +16,12 @@ import javax.servlet.http.HttpSession;
 public class MemberServiceAdvice {
     private Logger logger = LogManager.getLogger(MemberServiceAdvice.class);
 
-//    패키지.클래스.메서드(..) => (..) : 매개변수명시x 한개든 두개든 상관없다)
-    @Around("execution(* minha.hello.spring4.controller.MemberController.myinfo(..))")
+    //    * 패키지.클래스.메서드(..) => *은 모든 접근지정자, (..) : 매개변수명시x 한개든 두개든 상관없다)
+    @Pointcut("execution(* minha.hello.spring4.controller.MemberController.myinfo(..))")
+    public void myInfoPoint(){}
+    @Pointcut("execution(* minha.hello.spring4.controller.MemberController.join(..))")
+    public void joinPoint(){}
+    @Around("myInfoPoint()")
     public Object myinfoAOPProcess(ProceedingJoinPoint pjp) throws Throwable{
         logger.info("myinfoAOPProcess 호출");
         HttpSession sess = null;
@@ -29,6 +34,22 @@ public class MemberServiceAdvice {
         if(sess.getAttribute("member") == null)
             return "redirect:/member/login";
 //
+        Object obj = pjp.proceed();
+        return obj;
+    }
+    @Around("joinPoint()")
+    public Object joinAOPProcess(ProceedingJoinPoint pjp) throws Throwable{
+        logger.info("joinAOPProcess 호출");
+        HttpSession sess = null;
+        for (Object o:pjp.getArgs()) {
+            if(o instanceof HttpSession)
+                sess = (HttpSession) o;
+        }
+//        로그인 안 했다면 -> join으로 이동
+//        로그인 했다면 -> myinfo로 이동
+        if(sess.getAttribute("member") != null)
+            return "redirect:/member/myinfo";
+
         Object obj = pjp.proceed();
         return obj;
     }
